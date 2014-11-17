2deafc89bc53777f64ccb592dac1ff2caa0ae883
Marco Secreto <msegreto@miceover.com>
2014-11-16 20:15:44 -0800

    ex22

---
+++ ex22/ex22.h
@@ -0,0 +1,16 @@
+#ifndef _ex22_h
+#define _ex22_h
+
+// makes int available in other programs
+extern int THE_SIZE;
+
+// static internal vars
+int get_age();
+void set_age( int age);
+
+// updates static var insid eupdate ratio;
+double update_ratio(double ratio);
+
+void print_size();
+
+#endif
