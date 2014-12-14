116ce7636a3cb097e6b6bbb560475c50409d40d4
Marco Secreto <msegreto@miceover.com>
2014-12-13 19:35:11 -0800

    ex26 shell fixes
     Please enter the commit message for your changes. Lines starting

--- ex26/devpkg/shell.c
+++ ex26/devpkg/shell.c
@@ -12,7 +12,7 @@ int Shell_exec(Shell template, ...)
   const char *arg = NULL;
   int i = 0;
 
-  rv = apr_pool_create(&pool, NULL);
+  rv = apr_pool_create(&p, NULL);
   // Why equals success?
   check(rv == APR_SUCCESS, "Failed to create pool.");
 
@@ -20,12 +20,12 @@ int Shell_exec(Shell template, ...)
 
   for (key = va_arg(argp, const char *);
        key != NULL;
-       key = va_arg(argp, const char *)
+       key = va_arg(argp, const char *))
   {
     arg = va_arg(argp, const char *);
 
     for (i = 0; template.args[i] != NULL; i++) {
-      if (strcmp(template.args[i], key) {
+      if (strcmp(template.args[i], key)) {
         template.args[i] = arg;
         break;
       }
@@ -42,6 +42,40 @@ error:
     apr_pool_destroy(p);
   }
   return rc;
+}
+
+int Shell_run(apr_pool_t *p, Shell *cmd)
+{
+  apr_procattr_t *attr;
+  apr_status_t rv;
+  apr_proc_t newproc;
+
+  rv = apr_procattr_create(&attr, p);
+  check(rv == APR_SUCCESS, "Failed to create proc attr.");
+
+  rv = apr_procattr_io_set(attr, APR_NO_PIPE, APR_NO_PIPE,
+          APR_NO_PIPE);
+  check(rv == APR_SUCCESS, "Failed to set IO of command.");
+
+  rv = apr_procattr_dir_set(attr, cmd->dir);
+  check(rv == APR_SUCCESS, "Failed to set root to %s", cmd->dir);
+
+  rv = apr_procattr_cmdtype_set(attr, APR_PROGRAM_PATH);
+  check(rv == APR_SUCCESS, "Failed to set cmd type.");
+
+  rv = apr_proc_create(&newproc, cmd->exe, cmd->args, NULL, attr, p);
+  check(rv == APR_SUCCESS, "Failed to run command.");
+
+  rv = apr_proc_wait(&newproc, &cmd->exit_code, &cmd->exit_why, APR_WAIT);
+  check(rv == APR_CHILD_DONE, "Failed to wait.");
+
+  check(cmd->exit_code == 0, "%s exited badly.", cmd->exe);
+  check(cmd->exit_why == APR_PROC_EXIT, "%s was killed or crashed", cmd->exe);
+
+  return 0;
+
+error:
+  return -1;
 }
 
 Shell CLEANUP_SH = {
